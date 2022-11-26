eea3049b14551cd64f1c5061b73f6cf3ba897688
root <root@ml4.southamerica-east1-b.c.endless-datum-269518.internal>
2022-11-26 12:40:44 +0000

    changed request due mlflow update

--- Clase 5/04_bash_inference.sh
+++ Clase 5/04_bash_inference.sh
@@ -1,1 +1,5 @@
-curl http://127.0.0.1:5000/invocations -H 'Content-Type: application/json' -d '{  "data": [ [0,0,0,0]]}'
+curl http://127.0.0.1:5000/invocations -H 'Content-Type: application/json' -d '{
+  "dataframe_split": {
+      "data": [[10, 10, 10,10], [0,0, 0, 0]]
+  }
+}'
