cca97ee1f549f1c564881bcc9ff21e7410a3512d
Camilo <camiloamadio57@gmail.com>
2023-04-13 23:08:00 -0300

    changed connection scripts due release

--- Clase 6/00_database_connection_isolated.py
+++ Clase 6/00_database_connection_isolated.py
@@ -1,10 +1,24 @@
 from sqlalchemy import create_engine
+from sqlalchemy import text
 
-#To see the result of the query the method is fetchall
-#from sqlalchemy import create_engine
 
 #check this question to know the parameters https://stackoverflow.com/questions/3582552/what-is-the-format-for-the-postgresql-connection-string-url
-engine = create_engine('postgresql+psycopg2://postgres:?)T"L.GYq~FUdn]d@34.172.137.45:5432/postgres')
-connection = engine.connect()
-connection.execute(f"INSERT INTO events_data.predictions (user_id, predicted_revenue) VALUES ('{user_id}',{predicted_revenue})")
-#return predicted_revenue
+
+port = '5342'
+db_name = 'postgres'
+user = 'postgres'
+password = 'postgres'
+ip = 'localhost'
+
+#For remote dbs
+#engine = create_engine(f'postgresql+psycopg2://{user}:{password}@{ip}:{port}/{db_name}')
+
+#For localhost there is no port
+#engine = create_engine(f'postgresql+psycopg2://{user}:{password}@{ip}/{db_name}')
+
+id = 3
+nombre = "nombre"
+sql_insert = f"INSERT INTO public.tabla1 (id, nombre) VALUES ({id},'{nombre}')"
+with engine.begin() as connection:
+    x = connection.execute(text(sql_insert))
+
