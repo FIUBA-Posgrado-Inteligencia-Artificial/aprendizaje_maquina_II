71813ce38cdb16e1ec5035d652c4a6f922318006
Facundo Adrián Lucianna <facundolucianna@gmail.com>
2026-06-29 21:11:11 -0300

    refactor: migrate simple server implementation from Flask to FastAPI

--- clase2/hands-on/2-simple_server/README.md
+++ clase2/hands-on/2-simple_server/README.md
@@ -5,12 +5,12 @@ Si queremos usar la linea de comando, los pasos a seguir es
 1. Construimos la imagen
 
 ```Bash
-docker build -t hello-world-flask:latest . 
+docker build -t hello-world-fastapi:latest . 
 ```
 
 2. Ahora podemos ejecutar la imagen. Para esta imagen necesitamos conectar el puerto del host con el contendor asi 
 podemos acceder.
 
 ```Bash
-docker run -p 8000:8000 hello-world-flask
+docker run -p 8000:8000 hello-world-fastapi
 ```
--- clase2/hands-on/2-simple_server/app.py
+++ clase2/hands-on/2-simple_server/app.py
@@ -1,15 +1,17 @@
 import os
 
-from flask import Flask
+import uvicorn
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
-app = Flask(__name__)
+app = FastAPI()
 
 
-@app.route("/")
+@app.get("/", response_class=HTMLResponse)
 def hello():
     return "Hola mundo!!!"
 
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", "8000"))
-    app.run(debug=True, host="0.0.0.0", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=port)
